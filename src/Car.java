54aae548192f3a3792aa15f5a0adbb8c1477e531
MikhailEmv <cheburusshka0303@gmail.com>
2022-10-14 22:17:58 +0500

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+    private int speed;
+    public Car() {
+        this.speed = 10;
+    }
+    public Car(int speed) {
+        this.speed = speed;
+    }
+    public void writeMessage() {
+        System.out.printf("Hello, dude, now your speed is kind of %s km/h", speed);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        var mazda = new Car(1000);
+        var mrs = new Car();
+        mazda.writeMessage();
+    }
+}
